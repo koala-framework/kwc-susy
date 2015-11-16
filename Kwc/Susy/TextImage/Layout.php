ca789e03d7bc6fdd8839eba462a19cac7064d2e3
Niko Sams <ns@vivid-planet.com>
2015-11-16 14:30:48 +0100

    Fix image contexts

--- Kwc/Susy/TextImage/Layout.php
+++ Kwc/Susy/TextImage/Layout.php
@@ -11,22 +11,30 @@ class Kwc_Susy_TextImage_Layout extends Kwc_Susy_Layout
     {
         $ownContexts = parent::getChildContexts($data, $child);
 
-        $widthCalc = 100/$data->getComponent()->getRow()->image_width;
-        $ret = array();
-        $masterLayouts = Kwc_Susy_Helper::getLayouts();
-        foreach ($ownContexts as $context) {
-            $breakpoint = $masterLayouts[$context['masterLayout']][$context['breakpoint']];
-            //same logic in scss
-            if (!isset($breakpoint['breakpoint']) || (int)$breakpoint['breakpoint'] * $context['spans'] / $breakpoint['columns'] < 300) {
-                //full width
-            } else {
-                $context['spans'] = floor($context['spans'] * $widthCalc);
-                if ($context['spans'] < 1) {
-                    $context['spans'] = 1;
-                }
-                $ret[] = $context;
+        if ($child->id == 'image') {
+            if (!$data->getComponent()->getRow()->image || !$data->getComponent()->getRow()->image_width) {
+                return null;
             }
+
+            $widthCalc = 100/$data->getComponent()->getRow()->image_width;
+            $ret = array();
+            $masterLayouts = Kwc_Susy_Helper::getLayouts();
+            foreach ($ownContexts as $context) {
+                $breakpoint = $masterLayouts[$context['masterLayout']][$context['breakpoint']];
+                //same logic in scss
+                if (!isset($breakpoint['breakpoint']) || (int)$breakpoint['breakpoint'] * $context['spans'] / $breakpoint['columns'] < 300) {
+                    //full width
+                } else {
+                    $context['spans'] = floor($context['spans'] * $widthCalc);
+                    if ($context['spans'] < 1) {
+                        $context['spans'] = 1;
+                    }
+                    $ret[] = $context;
+                }
+            }
+            return $ret;
+        } else {
+            return $ownContexts;
         }
-        return $ret;
     }
 }
