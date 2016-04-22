9a0936f688169c6b3ae9b9b62fadc84c68168b24
Ricky Smith <rs@vivid-planet.com>
2016-04-22 11:21:56 +0200

    Extend Columns component with more possible combinations of columns

--- Susy/Kwc/Columns/Component.php
+++ Susy/Kwc/Columns/Component.php
@@ -5,25 +5,43 @@ class Susy_Kwc_Columns_Component extends Kwc_Columns_Abstract_Component
     public static function getSettings()
     {
         $ret = parent::getSettings('Kwc_Paragraphs_Component');
+        $ret['layoutClass'] = 'Susy_Kwc_Columns_Layout';
         $columnsTrl = trlKwfStatic('Columns');
+
         $ret['columns'] = array(
             '2col-50_50' => array(
-                'columns' => 2,
                 'colSpans' => array(1, 1),
                 'name' => "2 $columnsTrl (50% - 50%)"
             ),
+            '2col-66_33' => array(
+                'colSpans' => array(2, 1),
+                'name' => "2 $columnsTrl (66% - 33%)"
+            ),
+            '2col-33_66' => array(
+                'colSpans' => array(1, 2),
+                'name' => "2 $columnsTrl (33% - 66%)"
+            ),
+            '2col-75_25' => array(
+                'colSpans' => array(3, 1),
+                'name' => "2 $columnsTrl (75% - 25%)"
+            ),
+            '2col-25_75' => array(
+                'colSpans' => array(1, 3),
+                'name' => "2 $columnsTrl (25% - 75%)"
+            ),
             '3col-33_33_33' => array(
-                'columns' => 3,
                 'colSpans' => array(1, 1, 1),
                 'name' => "3 $columnsTrl (33% - 33% - 33%)"
             ),
+            '3col-25_50_25' => array(
+                'colSpans' => array(1, 2, 1),
+                'name' => "3 $columnsTrl (25% - 50% - 25%)"
+            ),
             '4col-25_25_25_25' => array(
-                'columns' => 4,
-                'colSpans' => array(1,1,1,1),
+                'colSpans' => array(1, 1, 1, 1),
                 'name' => "4 $columnsTrl (25% - 25% - 25% - 25%)"
             )
         );
-        $ret['layoutClass'] = 'Susy_Kwc_Columns_Layout';
         return $ret;
     }
 
--- Susy/Kwc/Columns/Layout.php
+++ Susy/Kwc/Columns/Layout.php
@@ -5,24 +5,28 @@ class Susy_Kwc_Columns_Layout extends Susy_Layout
     {
         $ret = array('child'=>array());
         $masterLayouts = Susy_Helper::getLayouts();
+
         foreach ($this->getSupportedContexts() as $context) {
             foreach ($this->_getSetting('columns') as $column) {
-                $breakpoint = $masterLayouts[$context['masterLayout']][$context['breakpoint']];
-                //same logic in scss
-                if (!isset($breakpoint['breakpoint']) || (int)$breakpoint['breakpoint'] * $context['spans'] / $breakpoint['columns'] < 300) {
-                    //full width
-                    $spans = $context['spans'];
-                } else {
-                    $spans = $context['spans'] / $column['columns'];
-                    $spans = floor($spans);
-                    if ($spans < 1) $spans = 1;
-                }
+                foreach ($column['colSpans'] as $span) {
+                    $breakpoint = $masterLayouts[$context['masterLayout']][$context['breakpoint']];
+                    //same logic in scss
+                    if (!isset($breakpoint['breakpoint']) || (int)$breakpoint['breakpoint'] * $context['spans'] / $breakpoint['columns'] < 300) {
+                        //full width
+                        $spans = $context['spans'];
+                    } else {
+                        $spans = $breakpoint['content-spans'] / array_sum($column['colSpans']) * $span;
+                        if ($spans < 1) $spans = 1;
+                    }
 
-                $ret['child'][] = array(
-                    'masterLayout' => $context['masterLayout'],
-                    'breakpoint' => $context['breakpoint'],
-                    'spans' => (int)$spans
-                );
+                    $newChild = array(
+                        'masterLayout' => $context['masterLayout'],
+                        'breakpoint' => $context['breakpoint'],
+                        'spans' => (int)$spans
+                    );
+
+                    if (!in_array($newChild, $ret['child'])) $ret['child'][] = $newChild;
+                }
             }
         }
 
@@ -32,10 +36,10 @@ class Susy_Kwc_Columns_Layout extends Susy_Layout
     public function getChildContexts(Kwf_Component_Data $data, Kwf_Component_Data $child)
     {
         $ownContexts = parent::getChildContexts($data, $child);
-
-        $widthCalc = $child->row->col_span / $child->row->columns;
-        $ret = array();
         $masterLayouts = Susy_Helper::getLayouts();
+
+        $ret = array();
+
         foreach ($ownContexts as $context) {
             $breakpoint = $masterLayouts[$context['masterLayout']][$context['breakpoint']];
             //same logic in scss
@@ -43,13 +47,18 @@ class Susy_Kwc_Columns_Layout extends Susy_Layout
                 //full width
                 $ret[] = $context;
             } else {
-                $context['spans'] = floor($context['spans'] * $widthCalc);
-                if ($context['spans'] < 1) {
-                    $context['spans'] = 1;
-                }
+                $colSettings = $this->_getSetting('columns');
+                $type = $data->getComponent()->getRow()->type;
+                $columnTypeSpans = $colSettings[$type]['colSpans'];
+                $span = $columnTypeSpans[$child->row->pos-1];
+
+                $context['spans'] = $breakpoint['columns'] / array_sum($columnTypeSpans) * $span;
+                if ($context['spans'] < 1) $context['spans'] = 1;
+
                 $ret[] = $context;
             }
         }
+
         return $ret;
     }
 }
