a2e398368803837aeb990e5748923bef44a58d5f
Benjamin Hohenwarter <bh@vivid-planet.com>
2019-10-15 09:08:54 +0200

    Anpassen von column-content-api response
    clients brauchen auch die information wie die spalten aufzuteilen sind

--- Susy/Kwc/Columns/ApiContent.php
+++ Susy/Kwc/Columns/ApiContent.php
@@ -4,9 +4,13 @@ class Susy_Kwc_Columns_ApiContent implements Kwf_Component_ApiContent_Interface
     public function getContent(Kwf_Component_Data $data)
     {
         $ret = array();
-        $ret['columnLayout'] = $data->getComponent()->getRow()->type;
-        $ret['columns'] = array();
+        $columnType = $data->getComponent()->getRow()->type;
+        $ret['columnType'] = $columnType;
 
+        $settingColumns = Kwc_Abstract::getSetting($data->componentClass, 'columns');
+        $ret['colSpans'] = $settingColumns[$columnType]['colSpans'];
+
+        $ret['columns'] = array();
         foreach ($data->getChildComponents(array('generator'=>'child')) as $columns) {
             $ret['columns'][] = $columns;
         }
